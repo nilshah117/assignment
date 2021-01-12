9a58e74d8c02109821c2ff7cb6fa4558e7567c50
nilshah117 <nilshah117@gmail.com>
2021-01-12 14:53:42 +0530

    hello world

---
+++ src/person.java
@@ -0,0 +1,15 @@
+public class person {
+	public static void main(String args[]){
+		System.out.println("");
+		
+		person p1 = new person();
+		System.out.println(p1);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "hello";
+	}
+	
+}
